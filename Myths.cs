3f18880cb8d4ac585aadea34cc4cc855f65cc0a5
Ann Johansson <ann.johansson.work@gmail.com>
2025-09-24 10:01:34 +0200

    Add project files.

---
+++ Myths.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentGithubOOP
+{
+    public class Myths // Base class
+    {
+        public string mythName; // Name of the myth
+        public string mythOrigin; // Origin of the myth
+        public string mythFact; // Interesting fact about the myth
+
+        public void writeOutMyth() // Method to write out the myth information
+        {
+            Console.WriteLine($"Now I am gonna tell you an interesting fact about {mythName}, a myth which originates from {mythOrigin}. {mythFact}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AssignmentGithubOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Myths testMyth = new Myths();
+
+            testMyth.writeOutMyth();
+        }
+    }
+}
