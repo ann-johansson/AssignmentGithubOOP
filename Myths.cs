32e16e0447dcb7a0a43b3a1c3808ddad4cd45465
Ann Johansson <ann.johansson.work@gmail.com>
2025-09-24 14:32:43 +0200

    I have made an encapsulation called mythSecret and also built a constructor for the attributes. Then I updated the method writeOutMyth to write out the new mythSecret class I made

--- Myths.cs
+++ Myths.cs
@@ -13,10 +13,22 @@ namespace AssignmentGithubOOP
         public string mythName; // Name of the myth
         public string mythOrigin; // Origin of the myth
         public string mythFact; // Interesting fact about the myth
+        private string mythSecret; // Private field for secret information, this is the encapsulation part
+
+
+        // Constructor to get the myth information
+        public Myths(string name, string origin, string fact, string secret)
+        {
+            mythName = name;
+            mythOrigin = origin;
+            mythFact = fact;
+            mythSecret = secret;
+        }
 
         public void writeOutMyth() // Method to write out the myth information
         {
-            Console.WriteLine($"Now I am gonna tell you an interesting fact about {mythName}, a myth which originates from {mythOrigin}. {mythFact}");
+            Console.WriteLine($"Now I am gonna tell you an interesting fact about {mythName}, a myth which originates from {mythOrigin}. {mythFact}" +
+                $" There's also a secret: {mythSecret}");
         }
     }
 }
