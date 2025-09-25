076deaf4aa2fc7fd659bae53f8933d71657e00ec
Ann Johansson <ann.johansson.work@gmail.com>
2025-09-25 20:33:59 +0200

    I have changed the Myths class into an abstract class and turned the method writeOutMyth into an abstract method. Then I created the Pontianak class and overrided the  writeOutMyth to a WriteLine. I then created an instance of the Pontianak class in my main, setted the properties and wrote out the information about the myth with my abstracted class.

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Pontianak thePontianak = new Pontianak(); // Create an instance of Pontianak class
+            Pontianak thePontianak = new Pontianak(); // Creating an instance of Pontianak class
 
-            // Setting properties of thePontianak object
+            // Setting properties of thePontianak instance
             thePontianak.mythName = "Pontianak";
             thePontianak.mythOrigin = "Indonesia and Malaysia";
             thePontianak.mythFact = "Pontianak is a vampiric female ghost born from women who died during childbirth.\n" +
-                "She appears as a pale, beautiful woman with long black hair and a flowery scent — but when she attacks,\n" +
-                "she turns monstrous, tearing out organs with her nails.\n" +
-                "Her presence is announced by the smell of frangipani flowers and a baby’s cry.\n" +
-                "If you hear the baby cry softly, she’s far away; if it’s loud, she’s right behind you.";
+                "She appears as a pale, beautiful woman with long black hair and a flowery scent — but when she attacks, \n" +
+                "she turns monstrous, tearing out organs with her nails." +
+                "\nHer presence is announced by the smell of frangipani flowers and a baby’s cry. " +
+                "\nIf you hear the baby cry softly, she’s far away; if it’s loud, she’s right behind you.";
 
             thePontianak.writeOutMyth(); // Writing out the myth information
         }
