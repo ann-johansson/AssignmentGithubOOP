3f52fc186ecc80547972a0d10d02842dbe621880
Ann Johansson <ann.johansson.work@gmail.com>
2025-09-25 19:15:41 +0200

    I added Virtual in the "Myths" class method, "writeOuteMythe" to make it a polymorphic method. *Then I added two new derived classes called "myth1Wendigo" and "myth2Nuckelavee" there I override the Write line into new more fitting messages for the output of information about each myth.

---
+++ myth1Wendigo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentGithubOOP
+{
+    public class myth1Wendigo : Myths
+    {
+
+        public override void writeOutMyth()
+        {
+            Console.WriteLine($"{mythName} is a myth that is {mythOrigin}. Some interesting facts is that: \n{mythFact}\n");
+        }
+
+    }
+}
---
+++ myth2Nuckelavee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentGithubOOP
+{
+    internal class myth2Nuckelavee : Myths
+    {
+        public override void writeOutMyth()
+        {
+            Console.WriteLine($"In {mythOrigin} there's also {mythName}. Here are some facts about {mythName}: \n{mythFact}");
+        }
+
+    }
+}
